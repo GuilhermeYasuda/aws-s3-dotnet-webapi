e64488984db45a08e6ff65bb53da9bc3d03f9f7a
Guilherme Harunobu Yasuda <haru_yasuda@hotmail.com>
2026-05-09 01:43:21 -0300

    feat: adiciona integração com S3 e controller de buckets
    Inclui configuração do serviço Amazon S3 via DI em Program.cs e cria o BucketsController com endpoints para criar, listar e deletar buckets utilizando o SDK da AWS. Implementa tratamento de erros e respostas apropriadas para cada operação.

---
+++ S3.Demo.API/Controllers/BucketsController.cs
@@ -0,0 +1,67 @@
+﻿using Amazon.S3;
+using Amazon.S3.Util;
+using Microsoft.AspNetCore.Mvc;
+
+namespace S3.Demo.API.Controllers
+{
+    [Route("api/buckets")]
+    [ApiController]
+    public class BucketsController : ControllerBase
+    {
+        private readonly IAmazonS3 _s3Client;
+
+        public BucketsController(IAmazonS3 s3Client)
+        {
+            _s3Client = s3Client;
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> CreateBucket(string bucketName)
+        {
+            bool bucketExists = await AmazonS3Util.DoesS3BucketExistV2Async(_s3Client, bucketName);
+            if (bucketExists) return BadRequest($"Bucket {bucketName} already exists.");
+
+            try
+            {
+                var response = await _s3Client.PutBucketAsync(bucketName);
+                return Ok($"Bucket '{bucketName}' created successfully.");
+            }
+            catch (AmazonS3Exception ex)
+            {
+                return BadRequest($"Error creating bucket: {ex.Message}");
+            }
+        }
+
+        [HttpGet("get-all")]
+        public async Task<IActionResult> GetAllBuckets()
+        {
+            try
+            {
+                var response = await _s3Client.ListBucketsAsync();
+                var buckets = response.Buckets.Select(b => { return b.BucketName; });
+                return Ok(buckets);
+            }
+            catch (AmazonS3Exception ex)
+            {
+                return BadRequest($"Error listing buckets: {ex.Message}");
+            }
+        }
+
+        [HttpDelete("delete")]
+        public async Task<IActionResult> DeleteBucket(string bucketName)
+        {
+            bool bucketExists = await AmazonS3Util.DoesS3BucketExistV2Async(_s3Client, bucketName);
+            if (!bucketExists) return NotFound($"Bucket {bucketName} does not exist.");
+
+            try
+            {
+                var response = await _s3Client.DeleteBucketAsync(bucketName);
+                return Ok($"Bucket '{bucketName}' deleted successfully.");
+            }
+            catch (AmazonS3Exception ex)
+            {
+                return BadRequest($"Error deleting bucket: {ex.Message}");
+            }
+        }
+    }
+}
--- S3.Demo.API/Program.cs
+++ S3.Demo.API/Program.cs
@@ -1,3 +1,4 @@
+using Amazon.S3;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +8,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+// carrega a configuração da AWS do arquivo appsettings.json para o ambiente de execução da aplicação
+builder.Services.AddDefaultAWSOptions(builder.Configuration.GetAWSOptions());
+// registra o serviço do Amazon S3 para ser injetado em outros componentes da aplicação para trabalhar com buckets e objetos
+builder.Services.AddAWSService<IAmazonS3>();
 
 var app = builder.Build();
 
